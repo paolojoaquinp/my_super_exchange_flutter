e9875173b69283e12072bc54cef1b9b05223ab67
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2025-10-12 05:17:23 -0400

    refactor: use camel case for mocked data

--- lib/features/home/data/mocked_data.dart
+++ lib/features/home/data/mocked_data.dart
@@ -1,70 +1,72 @@
-final homeData = {
-  'user': {
-    'name': 'Alexa Cubert',
-    'firstName': 'Alex',
-    'profileImage': 'https://picsum.photos/seed/alexa/200',
-    'notificationCount': 3,
-  },
-  'balance': {
-    'amount': 24000.98,
-    'currency': '\$',
-  },
-  'recentRecipients': [
-    {
-      'id': '1',
-      'name': 'Recipient 1',
-      'image': 'https://picsum.photos/seed/recipient1/200',
+// Simula la respuesta JSON de un backend real
+class MockedHomeData {
+  static final Map<String, dynamic> homeDataResponse = {
+    'user': {
+      'name': 'Alexa Cubert',
+      'firstName': 'Alex',
+      'profileImage': 'https://picsum.photos/seed/alexa/200',
+      'notificationCount': 3,
     },
-    {
-      'id': '2',
-      'name': 'Recipient 2',
-      'image': 'https://picsum.photos/seed/recipient2/200',
+    'balance': {
+      'amount': 24000.98,
+      'currency': '\$',
     },
-    {
-      'id': '3',
-      'name': 'Recipient 3',
-      'image': 'https://picsum.photos/seed/recipient3/200',
-    },
-    {
-      'id': '4',
-      'name': 'Recipient 4',
-      'image': 'https://picsum.photos/seed/recipient4/200',
-    },
-    {
-      'id': '5',
-      'name': 'Recipient 5',
-      'image': 'https://picsum.photos/seed/recipient5/200',
-    },
-  ],
-  'savings': [
-    {
-      'id': '1',
-      'name': 'Car',
-      'amount': 3074,
-      'target': 5000,
-      'color': 0xFF7B68EE,
-    },
-    {
-      'id': '2',
-      'name': 'Vacation',
-      'amount': 1500,
-      'target': 3000,
-      'color': 0xFF4ADE80,
-    },
-    {
-      'id': '3',
-      'name': 'New phone',
-      'amount': 600,
-      'target': 1200,
-      'color': 0xFFEF4444,
-    },
-    {
-      'id': '4',
-      'name': 'House',
-      'amount': 12023,
-      'target': 50000,
-      'color': 0xFF3B82F6,
-    },
-  ],
-};
-
+    'recentRecipients': [
+      {
+        'id': '1',
+        'name': 'Recipient 1',
+        'image': 'https://picsum.photos/seed/recipient1/200',
+      },
+      {
+        'id': '2',
+        'name': 'Recipient 2',
+        'image': 'https://picsum.photos/seed/recipient2/200',
+      },
+      {
+        'id': '3',
+        'name': 'Recipient 3',
+        'image': 'https://picsum.photos/seed/recipient3/200',
+      },
+      {
+        'id': '4',
+        'name': 'Recipient 4',
+        'image': 'https://picsum.photos/seed/recipient4/200',
+      },
+      {
+        'id': '5',
+        'name': 'Recipient 5',
+        'image': 'https://picsum.photos/seed/recipient5/200',
+      },
+    ],
+    'savings': [
+      {
+        'id': '1',
+        'name': 'Car',
+        'amount': 3074,
+        'target': 5000,
+        'color': 0xFF7B68EE,
+      },
+      {
+        'id': '2',
+        'name': 'Vacation',
+        'amount': 1500,
+        'target': 3000,
+        'color': 0xFF4ADE80,
+      },
+      {
+        'id': '3',
+        'name': 'New phone',
+        'amount': 600,
+        'target': 1200,
+        'color': 0xFFEF4444,
+      },
+      {
+        'id': '4',
+        'name': 'House',
+        'amount': 12023,
+        'target': 50000,
+        'color': 0xFF3B82F6,
+      },
+    ],
+  };
+}
